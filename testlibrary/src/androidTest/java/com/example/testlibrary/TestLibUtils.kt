b551290fda177ba5b9c80c8272c6a9bf91a87f2c
孟文钥 <527829255@qq.com>
2019-06-21 09:15:41 +0800

    sfirst

---
+++ testlibrary/src/androidTest/java/com/example/testlibrary/TestLibUtils.kt
@@ -0,0 +1,14 @@
+package com.example.testlibrary
+
+/**
+
+ * author ：MENG
+ * date : 2019/6/21 9:05
+ * package：com.example.testlibrary
+ * description :
+ */
+object TestLibUtils {
+    val a = "TextLib"
+
+
+}
---
+++ testlibrary/src/main/java/com/example/testlibrary/TestUtils.kt
@@ -0,0 +1,13 @@
+package com.example.testlibrary
+
+/**
+
+ * author ：MENG
+ * date : 2019/6/21 9:13
+ * package：com.example.testlibrary
+ * description :
+ */
+object TestUtils {
+
+    val utilds = "TestLib"
+}
